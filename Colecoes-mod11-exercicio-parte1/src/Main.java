36109ddefde78b4452e34f08f0d7260f96d80d29
RafaelCSO <rafaeljet00@gmail.com>
2023-05-05 15:57:51 -0300

    correção

--- Colecoes-mod11-exercicio-parte1/src/Main.java
+++ Colecoes-mod11-exercicio-parte1/src/Main.java
@@ -5,40 +5,33 @@ public class Main {
 
         Scanner s = new Scanner(System.in);
 
-        List<String> masculino = new ArrayList<>();
-        List<String> feminino = new ArrayList<>();
+        List<String> pessoas = new ArrayList<>();
         boolean continuar = true;
 
-        while(continuar){
-            System.out.print("Digite o nome e sexo separado por hifen: ");
+        while (continuar) {
+            System.out.print("Digite o nomes separados por virgula: ");
             String entradaNome = s.nextLine();
-            String[] entradaArray = entradaNome.split("-");
-            entradaArray[1] = entradaArray[1].toUpperCase().trim();
+            String[] entradaArray = entradaNome.split(",");
 
-            if(entradaArray[1].equalsIgnoreCase("M")){
-                masculino.add(entradaArray[0]);
-
-            }
-            else if (entradaArray[1].equalsIgnoreCase("F")){
-                feminino.add(entradaArray[0]);
-
+            for (String nome : entradaArray) {
+                pessoas.add(nome.trim());
             }
 
 
             System.out.println("Continuar ? Y/N");
             String novoNome = s.nextLine();
-            if (novoNome.equalsIgnoreCase("Y")){
+            if (novoNome.equalsIgnoreCase("Y")) {
                 continuar = true;
-            }
-            else {
+            } else {
                 continuar = false;
             }
-
+            Collections.sort(pessoas);
         }
 
-        Collections.sort(masculino);
-        Collections.sort(feminino);
-        System.out.println("Nomes Masculinos Ordenados: " + Arrays.toString(masculino.toArray()));
-        System.out.println("Nomes Femininos Ordenados: " + Arrays.toString(feminino.toArray()));
+        System.out.println("Nomes ordenados:");
+        for (String pessoa : pessoas) {
+            System.out.println(pessoa);
+        }
     }
-}
+
+    }
